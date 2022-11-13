06241ff94ff40097e6fb31f7fec11e0a5d687d9c
Antoine Lafarge <ant.lafarge@gmail.com>
2022-11-14 00:16:07 +0100

    Fix Base64

--- Base64/build-ppc64.sh
+++ Base64/build-ppc64.sh
@@ -3,10 +3,13 @@
 # sudo apt install g++-10-powerpc64-linux-gnu qemu-user
 # cd /mnt/c/Users/[username]/Documents/cpp-tools/Base64
 
-powerpc64-linux-gnu-g++-10 -Werror -Wall -Wextra -Wno-unknown-pragmas -g -Isrc src/*.cpp test.cpp -o test-ppc64.bin
+if [[ "$1" == "build" || $# -eq 0 ]]; then
+    echo "========= Build ========="
+    powerpc64-linux-gnu-g++-10 -Werror -Wall -Wextra -Wno-unknown-pragmas -g -Isrc src/*.cpp test/test.cpp -o test-ppc64.bin
+fi
 
-if [ $? -eq 0 ]
-then
+if [[ ($? -eq 0) && ("$1" == "run" || $# -eq 0) ]]; then
+    echo "========= Run ==========="
     export QEMU_LD_PREFIX=/usr/powerpc64-linux-gnu
     qemu-ppc64 ./test-ppc64.bin
 fi
