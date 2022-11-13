dacc50adb00ca64f7bb67d1cd404339c922577b3
Antoine Lafarge <ant.lafarge@gmail.com>
2022-11-14 00:11:29 +0100

    Fix Concurrent

--- Concurrent/build-g++.sh
+++ Concurrent/build-g++.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 # sudo apt install g++
-# cd /mnt/c/Users/[username]/Documents/cpp-tools/JSON
+# cd /mnt/c/Users/[username]/Documents/cpp-tools/Concurrent
 
-g++ -Werror -Wall -Wextra -Wno-unknown-pragmas -pthread -o test.bin *.cpp
-if [ "$1" == "-run" ]
-then
+if [[ "$1" == "build" || $# -eq 0 ]]; then
+    echo "========= Build ========="
+    g++ -Werror -Wall -Wextra -Wno-unknown-pragmas -pthread -Isrc test/test.cpp -o test.bin
+fi
+
+if [[ ($? -eq 0) && ("$1" == "run" || $# -eq 0) ]]; then
+    echo "========= Run ==========="
     ./test.bin
 fi
